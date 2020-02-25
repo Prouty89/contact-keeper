51a9bc8f14fc3a5ec9baa2c84faf6ca641bca623
Blake <bpro0821@gmail.com>
2020-02-25 08:55:18 -0600

    attach mongodb, receiving monitor connection err, begin user model

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
 // @route       POST   api/users
 // @desc        Resgister a user
 // @access      Public
 
 router.post('/', (req, res) => {
-    res.send('Register a user')
+    res.send(req.body)
 });
 
 module.exports = router;
