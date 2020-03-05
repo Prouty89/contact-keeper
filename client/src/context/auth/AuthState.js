367e796b19b7396618b8a30ae0be185a72a457db
Blake <bpro0821@gmail.com>
2020-03-05 13:48:50 -0600

    user registration and alerts for duplicates

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import authContext from'./authContext';
 import authReducer from './authReducer';
 import {
@@ -25,14 +26,39 @@ const AuthState = props => {
 
 
     // Load User
-
+    const loadUser = () => console.log('loadUser')
     // Register User
+    const register = async FormData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        try {
+            const res = await axios.post('/api/users', FormData, config);
+
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            });
+        } catch (err) {
+            dispatch({
+                type: REGISTER_FAIL,
+                payload: err.response.data.msg
+            });
+
+        }
+    }
 
     // Login User
+    const login = () => console.log('Login')
 
     // Logout
+    const logout = () => console.log('Logout')
 
     // Clear Errors
+    const clearErrors = () => dispatch({ type: CLEAR_ERRORS})
 
     return(
         <authContext.Provider
@@ -42,6 +68,11 @@ const AuthState = props => {
             loading: state.loading,
             user: state.user,
             error: state.error,
+            register,
+            loadUser,
+            login,
+            logout,
+            clearErrors
         }}
         >
         {props.children}
